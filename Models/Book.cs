8ecc1d28432f8b33d69e214038f41f81d1bf9ff0
Aleksandar <asp996@gmail.com>
2019-09-07 14:32:37 +0200

    Model implemenation

---
+++ Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookLibrary.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string Artist { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookLibrary.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string Name { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string State { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+        public User()
+        {
+            Books = new HashSet<Book>();
+        }
+
+    }
+}
